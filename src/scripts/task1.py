5a50f3ffa7d62f95392c18d2b0a210d01b298420
shaurya <shauryajha235@yahoo.com>
2025-02-12 20:47:15 +0530

    implemented P-Controller

--- src/scripts/task1.py
+++ src/scripts/task1.py
@@ -8,6 +8,7 @@ Implement a PID Controller for the second turtle to reach the goal.
 
 import rospy
 from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
 from turtlesim.srv import Spawn, Kill, SetPen, TeleportAbsolute
 import random
 import math
@@ -16,6 +17,7 @@ class Turtle:
     def __init__(self):
         rospy.init_node("turtle", anonymous=True)
 
+        # Setup services to mark goal, kill default turtle and spawn new turtle
         rospy.wait_for_service('kill')
         rospy.wait_for_service('spawn')
         rospy.wait_for_service('turtle1/set_pen')
@@ -26,12 +28,42 @@ class Turtle:
         self.set_pen = rospy.ServiceProxy('turtle1/set_pen', SetPen)
         self.teleport = rospy.ServiceProxy('turtle1/teleport_absolute', TeleportAbsolute)
 
+        # PID Controller parameters
+        self.Kp_linear = 1.0
+        self.Ki_linear = 0.0
+        self.Kd_linear = 0.0
+
+        self.Kp_angular = 1.0
+        self.Ki_angular = 0.0
+        self.Kd_angular = 0.0
+
+        # Error terms
+        self.prev_distance_error = 0.0
+        self.distance_error_integral = 0.0
+        self.prev_angle_error = 0.0
+        self.angle_error_integral = 0.0
+
+        # Goal coordinates
+        self.goal_x = 5.5
+        self.goal_y = 5.5
+
+        # Current pose data
+        self.current_pose = Pose()
+
+        # Setup publisher and subscriber
+        self.pub = rospy.Publisher('turtle2/cmd_vel', Twist, queue_size=10)
+        self.sub = rospy.Subscriber('turtle2/pose', Pose, self.pose_callback)
+
+    def pose_callback(self, data):
+        self.current_pose = data
+        self.move_to_goal()
+
     def mark_goal(self):
         try:
-            # set pen
+            # Set pen
             self.set_pen(255, 0, 0, 3, 0)
 
-            # draw the marker
+            # Draw the marker
             self.teleport(6, 6, math.pi/4)
             self.teleport(5, 5, math.pi/4)
             self.teleport(5.5, 5.5, math.pi/4)
@@ -40,7 +72,7 @@ class Turtle:
             self.teleport(5.5, 5.5, -math.pi/4)
             rospy.sleep(0.5)
 
-            # kill the turtle after marking the goal
+            # Kill the turtle after marking the goal
             rospy.loginfo(f"Goal marked; killing default turtle...")
             self.kill("turtle1")
             rospy.sleep(0.5)
@@ -54,12 +86,73 @@ class Turtle:
         theta = random.uniform(0, 2 * math.pi)
 
         try:
-            # spawn a turtle at a random location and log the spawn info
-            self.spawn(x, y, theta, "turtle1")
+            # Spawn a turtle at a random location and log the spawn info
+            self.spawn(x, y, theta, "turtle2")
             rospy.loginfo(f"Spawned turtle at x:{x:.2f}, y:{y:.2f}, theta:{theta:.2f}")
             rospy.sleep(0.5)
         except rospy.ServiceException as e:
             rospy.logerr(f"Failed to spawn turtle: {e}")
+
+    def calculate_distance_error(self):
+        # Euclidian Distance = ((x2-x1)^2 + (y2-y1)^2)^0.5
+        return math.sqrt((self.goal_x - self.current_pose.x)**2 + (self.goal_y - self.current_pose.y)**2)
+    
+    def calculate_angle_error(self):
+        # Slope of line between two points: arctan((y2-y1) / (x2-x1))
+        desired_angle = math.atan2((self.goal_y - self.current_pose.y), (self.goal_x - self.current_pose.x))
+        angle_error = desired_angle - self.current_pose.theta
+
+        # Normalise angle to [-pi, pi]
+        while angle_error > math.pi:
+            angle_error -= 2 * math.pi
+        while angle_error < -math.pi:
+            angle_error += 2 * math.pi
+
+        return angle_error
+    
+    def move_to_goal(self):
+        distance_error = self.calculate_distance_error()
+        angle_error = self.calculate_angle_error()
+
+        # Update integral terms
+        self.distance_error_integral += distance_error
+        self.angle_error_integral += angle_error
+
+        # Set derivative terms
+        distance_error_derivative = distance_error - self.prev_distance_error
+        angle_error_derivative = angle_error - self.prev_angle_error
+
+        # Use PID to calculate control signals
+        linear_velocity = (self.Kp_linear * distance_error)
+        + (self.Kd_linear * distance_error_derivative)
+        + (self.Ki_linear * self.distance_error_integral)
+
+        angular_velocity = (self.Kp_angular * angle_error)
+        + (self.Kd_angular * angle_error_derivative)
+        + (self.Ki_angular * self.angle_error_integral)
+
+        # Create a Twist message
+        cmd_vel = Twist()
+        cmd_vel.linear.x = linear_velocity
+        cmd_vel.angular.z = angular_velocity
+
+        # Publish command velocity and log current pose data
+        self.pub.publish(cmd_vel)
+        rospy.loginfo(f"Current pose data = x:{self.current_pose.x:.2f}, y:{self.current_pose.y:.2f}, theta:{self.current_pose.theta:.2f}")
+
+        # Update the previous error terms
+        self.prev_distance_error = distance_error
+        self.prev_angle_error = angle_error
+
+        # Stop at goal and log progress
+        '''
+        if distance_error < 0.1:
+            rospy.loginfo(f"Goal reached!")
+            rospy.loginfo(f"Current pose data = x:{self.current_pose.x:.2f}, y:{self.current_pose.y:.2f}, theta:{self.current_pose.theta:.2f}")
+            cmd_vel.linear.x = 0
+            cmd_vel.angular.z = 0
+            self.pub.publish(cmd_vel)
+        '''
 
 if __name__ == "__main__":
     try:
