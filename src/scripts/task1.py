94559b5ef1066a1ad89b8466f17235132b290ef2
shaurya <shauryajha235@yahoo.com>
2025-02-12 19:41:48 +0530

    drew marker for the goal

--- src/scripts/task1.py
+++ src/scripts/task1.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
+'''
+Use the default turtle to draw a red 'X' to mark the goal (center).
+Kill the default turtle and spawn a second turtle at a random point.
+Implement a PID Controller for the second turtle to reach the goal.
+'''
+
 import rospy
 from turtlesim.msg import Pose
-from turtlesim.srv import Spawn, Kill
+from turtlesim.srv import Spawn, Kill, SetPen, TeleportAbsolute
 import random
 import math
 
@@ -12,9 +18,33 @@ class Turtle:
 
         rospy.wait_for_service('kill')
         rospy.wait_for_service('spawn')
+        rospy.wait_for_service('turtle1/set_pen')
+        rospy.wait_for_service('turtle1/teleport_absolute')
 
-        self.kill_service = rospy.ServiceProxy('kill', Kill)
-        self.spawn_service = rospy.ServiceProxy('spawn', Spawn)
+        self.kill = rospy.ServiceProxy('kill', Kill)
+        self.spawn = rospy.ServiceProxy('spawn', Spawn)
+        self.set_pen = rospy.ServiceProxy('turtle1/set_pen', SetPen)
+        self.teleport = rospy.ServiceProxy('turtle1/teleport_absolute', TeleportAbsolute)
+
+    def mark_goal(self):
+        try:
+            # set pen
+            self.set_pen(255, 0, 0, 3, 0)
+
+            # draw the marker
+            self.teleport(6, 6, math.pi/4)
+            self.teleport(5, 5, math.pi/4)
+            self.teleport(5.5, 5.5, math.pi/4)
+            self.teleport(6, 5, -math.pi/4)
+            self.teleport(5, 6, -math.pi/4)
+            self.teleport(5.5, 5.5, -math.pi/4)
+
+            # kill the turtle after marking the goal
+            rospy.loginfo(f"Goal marked; killing default turtle...")
+            self.kill("turtle1")
+
+        except rospy.ServiceException as e:
+            rospy.logerr(f"Failed to mark goal and/or kill turtle: {e}")
 
     def spawn_turtle(self):
         x = random.uniform(1, 10)
@@ -22,11 +52,8 @@ class Turtle:
         theta = random.uniform(0, 2 * math.pi)
 
         try:
-            # kill default turtle spawned upon starting turtlesim node
-            self.kill_service("turtle1")
-
             # spawn a turtle at a random location and log the spawn info
-            self.spawn_service(x, y, theta, "turtle1")
+            self.spawn(x, y, theta, "turtle1")
             rospy.loginfo(f"Spawned turtle at x:{x:.2f}, y:{y:.2f}, theta:{theta:.2f}")
         except rospy.ServiceException as e:
             rospy.logerr(f"Failed to spawn turtle: {e}")
@@ -34,7 +61,9 @@ class Turtle:
 if __name__ == "__main__":
     try:
         turtle = Turtle()
+        turtle.mark_goal()
         turtle.spawn_turtle()
         rospy.spin()
+        
     except rospy.ROSInterruptException:
         pass
